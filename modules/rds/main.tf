2275e2ae505245d0007a3ff519ec7a9102765554
Matheus <matheusfernandesluz@gmail.com>
2024-09-28 14:21:48 -0400

    first commit.

---
+++ modules/rds/main.tf
@@ -0,0 +1,59 @@
+data "aws_vpc" "vpc" {
+  filter {
+    name   = "tag:Name"
+    values = ["tc-infra-cloud-vpc"]
+  }
+}
+
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.vpc.id]
+  }
+}
+
+resource "aws_db_subnet_group" "rds_subnet_group" {
+  name       = "rds-subnet-group"
+  subnet_ids = data.aws_subnets.default.ids
+}
+
+resource "aws_db_instance" "default" {
+  identifier = var.rds_cluster_identifier
+  allocated_storage    = 10
+  db_name              = "mydb"
+  engine               = "mysql"
+  engine_version       = "8.0"
+  instance_class       = "db.t3.micro"
+  username             = var.db_user
+  password             = var.db_password
+  port                 = var.db_port
+  parameter_group_name = "default.mysql8.0"
+  skip_final_snapshot  = true
+  #publicly_accessible  = true
+
+  db_subnet_group_name = aws_db_subnet_group.rds_subnet_group.name
+  vpc_security_group_ids = [aws_security_group.rds_sg.id]
+
+  tags = {
+    Name = var.rds_cluster_identifier
+  }
+}
+
+resource "aws_security_group" "rds_sg" {
+  name        = "rds-security-group"
+  vpc_id      = data.aws_vpc.vpc.id
+
+  ingress {
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"    
+    cidr_blocks = [var.vpc_cidr_block]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
